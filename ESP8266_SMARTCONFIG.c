66f290acbf3bebd0887ebebca6dfeacbdaede618
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-08-13 23:21:50 +0530

    simpliefied smartconfig features to work with ESP8266_SSID_FRAMEWORK library. wifi reconnect, wifi events etc would be handled by that library

--- ESP8266_SMARTCONFIG.c
+++ ESP8266_SMARTCONFIG.c
@@ -1,5 +1,4 @@
 /*************************************************
- *
  * ESP8266 SMARTCONFIG LIBRARY
  * (ESPTOUCH TYPE SUPPORTED ONLY)
  *
@@ -8,7 +7,7 @@
  *       IN THE PROJECT Makefile
  *
  * JUNE 29 2017
- * 
+ *
  * ANKIT BHATNAGAR
  * ANKIT.BHATNAGARINDIA@GMAIL.COM
  *************************************************/
@@ -19,72 +18,20 @@
 //DEBUG RELATRED
 static uint8_t _esp8266_smartconfig_debug;
 
-//WIFI CONNECTION RELATED
-static uint8_t _esp8266_smartconfig_wifi_connected;
-static os_timer_t _esp8266_smartconfig_wifi_timer;
-
-//OPERATIONAL MODE RELATED
-static ESP8266_SMARTCONFIG_MODE _esp8266_smartconfig_mode;
-static uint8_t _esp8266_smartconfig_gpio_num;
-
-//CALLBACK FUNCTION VARIABLES
-static void (*_esp8266_smartconfig_user_cb_function)(void);
-static void (*_esp8266_smartconfig_smartconfig_activated_user_cb_function)(void);
-static void (*_esp8266_smartconfig_smartconfig_not_activated_user_cb_function)(void);
-
 void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_SetDebug(uint8_t debug_on)
 {
     //SET DEBUG PRINTF ON(1) OR OFF(0)
-    
+
     _esp8266_smartconfig_debug = debug_on;
 }
 
-void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Initialize(ESP8266_SMARTCONFIG_MODE mode, uint8_t gpio)
+void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Initialize(void)
 {
-    //INITIALIZE SMARTCONFIG IN THE MODE SPECIFIED
-    //GPIO : GPIO PIN READ VALUE IS USED TO DETERMINE IF BOOTING IN THE NORMAL MODE OR SMARTCONFIG
-    //WIFI_TIMEOUT : IF CONNECTION TO WIFI FAILS(USING STORED CREDENTIALS), START SMARTCONFIG
-    
-    _esp8266_smartconfig_mode = mode;
-    _esp8266_smartconfig_gpio_num = gpio;
-
-    if(mode == ESP8266_SMARTCONFIG_MODE_GPIO)
-    {
-        //GPIO MODE
-
-        //SET THE GPIO PIN TO INPUT WILL PULLUP ENABLED
-        _esp8266_smartconfig_set_gpio_pin(gpio);
-
-        if(_esp8266_smartconfig_debug)
-        {
-            os_printf("ESP8266 : SMARTCONFIG : Initialized in GPIO mode on pin %u\n", _esp8266_smartconfig_gpio_num);
-        }
-    }
-    else
-    {
-        //WIFI TIMEOUT MODE
-        
-        if(_esp8266_smartconfig_debug)
-        {
-            os_printf("ESP8266 : SMARTCONFIG : Initialized in WIFI TIMEOUT mode\n");
-        }
-    }
-
-    _esp8266_smartconfig_wifi_connected = 0;
-}
-
-void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_SetCbFunctions(void (*sc_done)(void), void (*sc_activated)(void), void (*sc_not_activated)(void))
-{
-    //SET THE USER CB FUNCTION THAT WOULD BE CALLED
-    //WHEN SMARTCONFIG IS FINISHED SUCCESSFULLY
-
-    _esp8266_smartconfig_user_cb_function = sc_done;
-    _esp8266_smartconfig_smartconfig_activated_user_cb_function = sc_activated;
-    _esp8266_smartconfig_smartconfig_not_activated_user_cb_function = sc_not_activated;
+    //INITIALIZE SMARTCONFIG
 
     if(_esp8266_smartconfig_debug)
     {
-        os_printf("ESP8266 : SMARTCONFIG : User cb functions set\n");
+        os_printf("ESP8266 : SMARTCONFIG : Initialized\n");
     }
 }
 
@@ -92,205 +39,24 @@ void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_SetCbFunctions(void (*sc_done)(void),
 void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Start(void)
 {
     //START SMARTCONFIG
-    //DEPENDING ON THE OPERATIONAL MODE, EITHER USE GPIO TO DETERMINE
-    //IF TO START IN SMARTCONFIG MODE OR USE WIFI TIMEOUT
-    
+
     if(_esp8266_smartconfig_debug)
     {
         os_printf("ESP8266 : SMARTCONFIG : Started\n");
     }
 
-    //SET WIFI EVENTS CB FUNCTION
-    wifi_set_event_handler_cb(_esp8266_smartconfig_wifi_event_handler_cb);
+    //SET THE SYSTEM IN STATION MODE - SMARTCONFIG REQUIREMENT
+    wifi_set_opmode(STATION_MODE);
 
-    if(_esp8266_smartconfig_mode == ESP8266_SMARTCONFIG_MODE_GPIO)
-    {
-        //GPIO MODE
-
-        if(_esp8266_smartconfig_get_gpio_reading(_esp8266_smartconfig_gpio_num) == 0)
-        {
-            //GPIO PRESSED. START SMARTCONFIG
-
-            //CALL THE SMARTCONFIG ACTIVATED USER CB IF NOT NULL
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : Smartconfig activated user cb executed\n");
-            }
-            
-            if(_esp8266_smartconfig_smartconfig_activated_user_cb_function != NULL)
-            {
-                (*_esp8266_smartconfig_smartconfig_activated_user_cb_function)();
-            }
-
-            //SET THE SYSTEM IN STATION MODE - SMARTCONFIG REQUIREMENT
-            wifi_set_opmode(STATION_MODE);
-
-            //START SMARTCONFIG
-            smartconfig_start(_esp8266_smartconfig_smartconfigevents_cb, SC_TYPE_ESPTOUCH);
-            
-        }
-        else
-        {
-            //GPIO NOT PRESSED. NORMAL BOOT MODE
-            
-            //CALL THE SMARTCONFIG NOT ACTIVATED USER CB IF NOT NULL
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : Smartconfig not activated user cb executed\n");
-            }
-            
-            if(_esp8266_smartconfig_smartconfig_not_activated_user_cb_function != NULL)
-            {
-                (*_esp8266_smartconfig_smartconfig_not_activated_user_cb_function)();
-            }
-
-            wifi_station_connect();
-        }
-    }
-    else
-    {
-        //WIFI TIMEOUT MODE
-    }
-}
-
-//INTERNAL GPIO FUNCTIONS
-void ICACHE_FLASH_ATTR _esp8266_smartconfig_set_gpio_pin(uint8_t gpio_num)
-{
-    //SET THE SPECIFIED GPIO PIN MODE TO INPUT + PULL UP
-    
-    switch(gpio_num)
-    {
-        case 0:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO0_U, FUNC_GPIO0);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO0_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(0));
-            break;
-        case 2:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO2_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(2));
-            break;
-        case 4:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO4_U, FUNC_GPIO4);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO4_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(4));
-            break;
-        case 5:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO5_U, FUNC_GPIO5);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_GPIO5_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(5));
-            break;
-        case 12:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_MTDI_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(12));
-            break;
-        case 13:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_MTCK_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(13));
-            break;
-        case 14:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, FUNC_GPIO14);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_MTMS_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(14));
-            break;
-        case 15:
-            PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, FUNC_GPIO15);
-            PIN_PULLUP_EN(PERIPHS_IO_MUX_MTDO_U);
-            GPIO_DIS_OUTPUT(GPIO_ID_PIN(15));
-            break;
-        default:
-            os_printf("ESP8266 : SMARTCONFIG : gpio set not supported for %d\n", gpio_num);
-    }
-    
-    if(_esp8266_smartconfig_debug)
-    {
-        os_printf("ESP8266 : SMARTCONFIG : Set gpio %d to input + pullup\n", gpio_num);
-    }
-}
-
-uint8_t ICACHE_FLASH_ATTR _esp8266_smartconfig_get_gpio_reading(uint8_t gpio_num)
-{
-    //RETURN THE READING OF THE SPECIFIED GPIO PIN
-
-    switch(gpio_num)
-    {
-        case 0:
-        case 2:
-        case 4:
-        case 5:
-        case 12:
-        case 13:
-        case 14:
-        case 15:
-            return GPIO_INPUT_GET(gpio_num);
-            break;
-        default:
-            os_printf("ESP8266 : SMARTCONFIG : gpio read value not supported for %d\n", gpio_num);
-    }
+    //START SMARTCONFIG
+    smartconfig_start(_esp8266_smartconfig_smartconfigevents_cb, SC_TYPE_ESPTOUCH);
 }
 
 //INTERNAL CB FUNCTIONS
-void ICACHE_FLASH_ATTR _esp8266_smartconfig_wifi_event_handler_cb(System_Event_t* event)
-{
-    //INTERNAL CB FUNCTION FOR WIFI EVENTS
-    
-    switch(event->event)
-    {
-        case EVENT_STAMODE_CONNECTED:
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : wifi event CONNECTED\n");
-            }
-            break;
-        case EVENT_STAMODE_DISCONNECTED:
-            _esp8266_smartconfig_wifi_connected = 0;
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : wifi event DISCONNECTED\n");
-            }
-            break;
-        case EVENT_STAMODE_AUTHMODE_CHANGE:
-           if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : wifi event AUTHMODE_CHANGE\n");
-            } 
-            break;
-        case EVENT_STAMODE_GOT_IP:
-            //DEVICE CONNECTED TO WIFI. CALL USER CB FUNCTION
-            //TO START THE APPLICATION
-            //CALL USER CB IF NOT NULL
-            _esp8266_smartconfig_wifi_connected = 1;
-            if(_esp8266_smartconfig_user_cb_function != NULL)
-            {
-                (*_esp8266_smartconfig_user_cb_function)();
-            }
-            break;
-        case EVENT_SOFTAPMODE_STACONNECTED:
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : wifi event SOFTAP STA CONNECTED\n");
-            } 
-            break;
-        case EVENT_SOFTAPMODE_STADISCONNECTED:
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : wifi event SOFTAO STA DISCONNECTED\n");
-            } 
-            break;
-        default:
-            if(_esp8266_smartconfig_debug)
-            {
-                os_printf("ESP8266 : SMARTCONFIG : Unknow wifi event %d\n", event->event);
-            }
-    }
-}
-
 void ICACHE_FLASH_ATTR _esp8266_smartconfig_smartconfigevents_cb(sc_status status, void* pdata)
 {
     //INTERNAL CB FUNCTION FOR SMARTCONFIG EVENTS
-    
+
     switch(status)
     {
         case SC_STATUS_WAIT:
@@ -324,8 +90,6 @@ void ICACHE_FLASH_ATTR _esp8266_smartconfig_smartconfigevents_cb(sc_status statu
             struct station_config* sta_conf = pdata;
             wifi_station_set_config(sta_conf);
             wifi_station_disconnect();
-            
-            wifi_set_event_handler_cb(_esp8266_smartconfig_wifi_event_handler_cb);
             wifi_station_connect();
 
             //SET AUTO CONNECT = ON
@@ -355,9 +119,9 @@ void ICACHE_FLASH_ATTR _esp8266_smartconfig_smartconfigevents_cb(sc_status statu
                 //ESP_AIRKISS TYPE
                 //NOT SUPPORTED - DONT DO ANYTHING
             }
-            
+
             smartconfig_stop();
-            
+
             //SMARTCONFIG DONE
             if(_esp8266_smartconfig_debug)
             {
@@ -366,4 +130,3 @@ void ICACHE_FLASH_ATTR _esp8266_smartconfig_smartconfigevents_cb(sc_status statu
             break;
     }
 }
-
--- ESP8266_SMARTCONFIG.h
+++ ESP8266_SMARTCONFIG.h
@@ -1,5 +1,4 @@
 /*************************************************
- *
  * ESP8266 SMARTCONFIG LIBRARY
  * (ESPTOUCH TYPE SUPPORTED ONLY)
  *
@@ -8,7 +7,7 @@
  *       IN THE PROJECT Makefile
  *
  * JUNE 29 2017
- * 
+ *
  * ANKIT BHATNAGAR
  * ANKIT.BHATNAGARINDIA@GMAIL.COM
  *************************************************/
@@ -24,32 +23,17 @@
 #include "mem.h"
 #include "smartconfig.h"
 
-#define ESP8266_SMARTCONFIG_WIFI_RETRY_COUNT    3
-
 //CUSTOM VARIABLE STRUCTURES/////////////////////////////
-typedef enum
-{
-  ESP8266_SMARTCONFIG_MODE_GPIO,
-  ESP8266_SMARTCONFIG_MODE_WIFITIMEOUT
-} ESP8266_SMARTCONFIG_MODE;
 
 //FUNCTION PROTOTYPES/////////////////////////////////////
 //CONFIGURATION FUNCTIONS
 void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_SetDebug(uint8_t debug_on);
-void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Initialize(ESP8266_SMARTCONFIG_MODE mode, uint8_t gpio);
-void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_SetCbFunctions(void (*sc_done)(void), void (*sc_activated)(void), void (*sc_not_activated)(void));
+void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Initialize(void);
 
 //CONTROL FUNCTIONS
 void ICACHE_FLASH_ATTR ESP8266_SMARTCONFIG_Start(void);
 
-//INTERNAL GPIO FUNCTIONS
-void ICACHE_FLASH_ATTR _esp8266_smartconfig_set_gpio_pin(uint8_t gpio_num);
-uint8_t ICACHE_FLASH_ATTR _esp8266_smartconfig_get_gpio_reading(uint8_t gpio_num);
-
 //INTERNAL CALLBACK FUNCTIONS
-void ICACHE_FLASH_ATTR _esp8266_smartconfig_wifi_event_handler_cb(System_Event_t* event);
 void ICACHE_FLASH_ATTR _esp8266_smartconfig_smartconfigevents_cb(sc_status status, void* pdata);
 //END FUNCTION PROTOTYPES/////////////////////////////////
 #endif
-
-
